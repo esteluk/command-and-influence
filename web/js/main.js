b3ab647dbb80a9487950dee029e09d246502870a
Stuart McAlpine <galbustein@gmail.com>
2013-06-23 00:27:12 +0100

    final comit of the night

--- web/js/main.js
+++ web/js/main.js
@@ -1,9 +1,15 @@
+var helper; // the CBHelper object
+var map; // The Google maps object
+var markers; // The Google maps markers
+var currentPosition; // The shared current position object
+var pushToken; // A push notification token for the device
+
 $(document).ready(function() {
 
 	/*
-	 * ***********************
-	 * jQuery & 'normal' stuff
-	 * ***********************
+	 * *********************
+	 * 'normal' jQuery stuff
+	 * *********************
 	 */
 
     $(window).resize(function() {
@@ -23,34 +29,118 @@ $(document).ready(function() {
 	 * ***********
 	 */
 
-	var map;
-
 	function initialize() {
+
+		/*
+		 * ***************
+		 * Cloudbase setup
+		 * ***************
+		 * 
+		 * We do this within the Google maps initialise() so we can get access to the commander latlong
+		 *
+		 */
+
+		// initialise the helper object with the code, secret code and the
+		// generic helper
+		helper = new CBHelper(
+		    "commandandinfluence", 
+		    "17b41ff67e279f762ba20a5bbc3fa959", 
+		    new GenericHelper()
+		);
+
+		// use the md5 library provided to set the password
+		helper.setPassword(hex_md5("76Indnja"));
+
+		// we only do searches based on proximity, not keywords
+		var search = {
+	//        'cb_location' : {
+	//            '$near' : [ currentPosition.lat, currentPosition.lng ],
+	//            // 5km (5000 meters converted to radians)
+	//            '$maxDistance' :  5/111.12 
+	//        },
+	//        'cb_limit' : 26
+		};
+
+		// call the searchDocuments function
+		helper.searchDocuments(search, "user", function(resp) {
+		    console.log(resp);
+		    console.log(resp.outputString);
+
+		    var json  = $.parseJSON(resp.outputString);
+
+		    // add markers for your troops
+		    $(json.data.message).each(function(index, value){
+		        console.log('value', value);
+		        update_marker(value.cb_location.lat, value.cb_location.lng, value.last_name);
+		    });
+
+		});
+
 	    var mapOptions = {
-	        zoom: 19,
-	        mapTypeId: google.maps.MapTypeId.TERRAIN,
-	        streetViewControl: false
+	        zoom: 20,
+//	        mapTypeId: google.maps.MapTypeId.TERRAIN,
+	        streetViewControl: false //,
+//	        maxZoom: 22,
+//	        minZoom: 12
 	    };
 	    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
 	    // Try HTML5 geolocation
 	    if (navigator.geolocation) {
+
 	        navigator.geolocation.getCurrentPosition(function(position) {
 	            var pos = new google.maps.LatLng(
 	                position.coords.latitude,
 	                position.coords.longitude
 	            );
 
-	            var infowindow = new google.maps.InfoWindow({
-	                map: map,
-	                position: pos,
-	                content: 'Location found using HTML5.'
-	            });
+//	            var infowindow = new google.maps.InfoWindow({
+//	                map: map,
+//	                position: pos,
+//	                content: 'You are here.'
+//	            });
+
+				new google.maps.Marker({
+					position: pos,
+					map: map,
+	                animation: google.maps.Animation.BOUNCE
+				});
 
 	            map.setCenter(pos);
+
+				currentPosition = new CBHelperCurrentLocation(position.coords.latitude, position.coords.longitude, position.coords.altitude);
+				helper.currentLocation = currentPosition;
+
+//				var pushManager = new PushNotificationManager();
+				
+//				pushManager.register(function(token) {
+//					// subscribe the current device to the push notification channel
+//					pushToken = token;
+//				});
+
 	        }, function() {
 	            handleNoGeolocation(true);
 	        });
+
+
+	        			// grabs the new position and updates the map
+			function updateLocation() {
+				navigator.geolocation.getCurrentPosition(
+					function (position) {
+						mosync.rlog("received position: lat: " + position.coords.latitude + " lng: " + position.coords.longitude);
+						// set the current location in the helper class
+						// re-center the map
+						var newCenter = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+						map.setCenter(newCenter);
+						// update friends locations
+						updateLocations();
+					},
+					function (error) {
+						mosync.rlog("Error while updating location");
+						mosync.rlog(error.message);
+					}
+				);
+			}
 
 	    } else {
 	        // Browser doesn't support Geolocation
@@ -77,24 +167,30 @@ $(document).ready(function() {
 
 	google.maps.event.addDomListener(window, 'load', initialize);
 
-	function update_marker(latitude, longitude) {
+	function update_marker(latitude, longitude, surname) {
 
 	    // grab a random colour and letter
-	    var colour = markers.colours[Math.floor(Math.random() * markers.colours.length)];
-	    var letter = markers.letters[Math.floor(Math.random() * markers.letters.length)];
-
-	    markers.sample_latlongs[Math.floor(Math.random() * 3)];
+//	    var colour = markers.colours[Math.floor(Math.random() * markers.colours.length)];
+	    var colour = 'darkgreen';
+	    console.log(surname);
+	    var letter = surname[0].toUpperCase();
 
 	    // add marker
-	    new google.maps.Marker({
+	    markers[surname] = new google.maps.Marker({
 	        position: new google.maps.LatLng(latitude, longitude),
 	        map: map,
 	        title: latitude + ', ' + longitude,
-	        icon: markers.directory + colour + '_marker' + letter + '.png'
+	        icon: markers.directory + colour + '_marker' + letter + '.png',
+	        draggable: true
 	    });
+
+		google.maps.event.addListener(markers[surname], 'click', function() {
+			console.log('clicked marker', surname);
+		});
 
 	}
 
+	// @todo associate users with letters (first letter of first name?) and colours
 	var markers = {
 	    directory: '/img/markers/',
 	    colours: [
@@ -104,11 +200,6 @@ $(document).ready(function() {
 	    letters: [
 	        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
 	        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-	    ],
-	    sample_latlongs: [
-	        [51.508318430739585, -0.13164818286895752],
-	        [51.507864349067205, -0.13120830059051514],
-	        [51.508318430739585, -0.13216853141784668]
 	    ]
 	}
 
@@ -190,47 +281,8 @@ $(document).ready(function() {
     // client-location
     channel.bind('client-location', function(data) {
         console.log('client-location', data);
-        update_marker(data.location.latitude, data.location.longitide);
+        // surname this is hardcoded for now until we handle marker add/update
+        update_marker(data.location.latitude, data.location.longitide, 'McAlpine');
     });
-
-	/*
-	 * *********
-	 * Cloudbase
-	 * *********
-	 */
-
-	// initialise the helper object with the code, secret code and the
-	// generic helper
-	var helper = new CBHelper(
-	    "commandandinfluence", 
-	    "17b41ff67e279f762ba20a5bbc3fa959", 
-	    new GenericHelper()
-	);
-
-	// use the md5 library provided to set the password
-	helper.setPassword(hex_md5("76Indnja"));
-
-	// we only do searches based on proximity, not keywords
-	var search = {
-	//            'cb_location' : {
-	//                '$near' : [ currentPosition.lat, currentPosition.lng ],
-	//                // 5km (5000 meters converted to radians)
-	//                '$maxDistance' :  5/111.12 
-	//            },
-	//            'cb_limit' : 26
-	};
-	// call the searchDocuments function
-	helper.searchDocuments(search, "user", function(resp) {
-	    console.log(resp);
-	    console.log(resp.outputString);
-
-	    var json  = $.parseJSON(resp.outputString);
-
-	    $(json.data.message).each(function(i, e){
-	        console.log('e', e);
-	        // update_marker(data.location.latitude, data.location.longitide);
-	    });
-
-	});
 
 });
